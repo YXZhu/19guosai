d30a3ee3ae8983d25e2ae9f0f1d44fc2cb5a5846
SCAUofLGC <2635084584@qq.com>
2019-07-21 11:20:58 +0800

    deleted .d .axf .lnp ...

--- MDK-ARM/19guosai/ringbuff.d
+++
@@ -1,4 +0,0 @@
-19guosai\ringbuff.o: ..\Middlewares\Third_Party\ringbuff\src\ringbuff\ringbuff.c
-19guosai\ringbuff.o: ..\Middlewares\Third_Party\ringbuff\src\include\ringbuff/ringbuff.h
-19guosai\ringbuff.o: D:\software\Keil\ARM\ARMCC\Bin\..\include\string.h
-19guosai\ringbuff.o: D:\software\Keil\ARM\ARMCC\Bin\..\include\stdint.h
